d1ac948d0c0f62c225be81ade21f2809b0ee23ff
LOUIS Afful <123727115+louisafful1@users.noreply.github.com>
2023-08-04 15:18:15 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,90 +1,75 @@
 <?php
 include 'include/database.php';
 if(isset($_POST['submit'])){
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-    $confirm = $_POST['confirm'];
+	$name = test_input($_POST['name']);
+	$email = test_input($_POST['email']);
+	$username = test_input($_POST['username']);
+	$password = test_input($_POST['password']);
+    $confirm = test_input($_POST['confirm']);
 
 	$sql1 = mysqli_query($connection, "INSERT INTO users VALUES(DEFAULT, '$name', '$email', '$username', '$password')");
    $sql2 = mysqli_query($connection, "SELECT * FROM users WHERE email='$email'");
   $row = mysqli_num_rows($sql2);
    
 
-   if($sql1){
-   		echo "<script>alert('Successfully Signed up');
-   		document.location='login.php';
-</script>";
+
+if(!$name){
+	echo "<p style='color:red;'>This filled is required</p>";
+} elseif($sql1){
+   		echo "<p style='color:green;'>Successfully Signed up</p>";
+   		// sleep(3000);
+   		// echo "<script> document.location='login.php';</script>";
    }
    else{
-   	echo "<script>alert('Sorry, this email already exist')</script>";
-//    } else if($password!= '' && $confirm != ''){
-// 		 	echo "<script>alert('The fields are empty')</script>";
-// 			} else if($password != $confirm){
-// echo "<script>Sorry, The Passwords do not match</script>";
-// 		} else echo "<script>Successfully Signed up</script>";
+   	echo "<p style='color:red;'>Sorry, this email already exist</p>";
 
 }
-   } 
+   }
 
 
-
-
+   function test_input($data){
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	
+	return $data;
+	}
+	
 
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Signup</title>
+<link rel="stylesheet" type="text/css" href="css/signup.css">
 
-	<style type="text/css">
-		form{
-			margin-top: 120px; 
-			margin-left:500px; 
-			width: 25%;
-			border: 2px solid black;
-		}
-
-		.adjust{
-			margin-left: 20px;
-		}
-	</style>
 </head>
 <body>
 
 <form action="" method="post" name="signup">
 	 <center><h1>Signup</h1></center>
-	<label class="adjust">Name</label><br>
-	<input class="adjust" type="text" name="name"><br><br>
+	<label>Name</label><br>
+	<input class="input" type="text" name="name"><br><br>
 
-	<label class="adjust">Email</label><br>
-	<input class="adjust" type="email" name="email"><br><br>
+	<label>Email</label><br>
+	<input class="input" type="email" name="email"><br><br>
 
-	<label class="adjust">Username</label><br>
-	<input class="adjust" type="text" name="username"><br><br>
+	<label>Username</label><br>
+	<input class="input" type="text" name="username"><br><br>
 
-	<label class="adjust" id="password">Password</label><br>
-	<input class="adjust" type="password" name="password" required><br><br>
+	<label id="password">Password</label><br>
+	<input class="input" type="password" name="password"><br><br>
 
-	<label class="adjust" id="confirm">Confirm Password</label><br>
-	<input class="adjust" type="password" name="confirm" required><br><br>
+	<label id="confirm">Confirm Password</label><br>
+	<input class="input" type="password" name="confirm"><br><br>
 
-	<input class="adjust" type="submit" name="submit" onclick="test(signup)"><br>
-	<p align="center">already have an account, login now? <a href="login.php">Login now</a></p>
+	<input class=" register adjust" type="submit" name="submit" value="Signup" onclick="test(signup)"><br>
+	<p align="center">already have an account, login now? <a href="login.php">login now</a></p>
 
 </form><br><br>
 <script type="text/javascript">
-	function test(signup){
-		if(signup.password.value!=signup.confirm.value){
-			alert("Please check the password very well!");
-		 }else{
-		 	signup.submit();
-		 }
-	}
+
 </script>
 </body>
-</html>
+</html
