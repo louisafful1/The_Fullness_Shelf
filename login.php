0e5479b51c343a823a74f6c12757520219c7638a
LOUIS Afful <123727115+louisafful1@users.noreply.github.com>
2023-08-04 15:16:48 +0000

    Update login.php

--- login.php
+++ login.php
@@ -2,13 +2,11 @@
  session_start();
 include 'include/database.php';
 if(isset($_POST['submit'])){;
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-
-
+	$username = test_input($_POST['username']);
+	$password = test_input($_POST['password']);
 	
-   $sql4 = mysqli_query($connection, "SELECT * FROM users WHERE username='$username' and password = '$password'");
-  $row = mysqli_fetch_array($sql4);
+$sql4 = mysqli_query($connection, "SELECT * FROM users WHERE username='$username' and password = '$password'");
+ $row = mysqli_fetch_array($sql4);
    
 
    if($row>0){
@@ -22,51 +20,37 @@ header("location:homepage.php");
    	echo "<script> alert('Invalid username or Password');</script>";
    }
   
-//    } else if($password!= '' && $confirm != ''){
-// 		 	echo "<script>alert('The fields are empty')</script>";
-// 			} else if($password != $confirm){
-// echo "<script>Sorry, The Passwords do not match</script>";
-// 		} else echo "<script>Successfully Signed up</script>";
 
-} 
-
-
-
+}
+function test_input($data){
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	
+	return $data;
+	}
+	
 
 
 ?>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="css/Login.css">
 
-	<style type="text/css">
-		form{
-			margin-top: 120px; 
-			margin-left:500px; 
-			width: 25%;
-			border: 2px solid black;
-		}
-
-		.adjust{
-			margin-left: 20px;
-		}
-	</style>
 </head>
 <body>
 
 <form action="" method="post" name="signup">
 	 <center><h1>User Login</h1></center>
-	<label class="adjust">Username</label><br>
-	<input class="adjust" type="text" name="username"><br><br>
+	<label>Username</label><br>
+	<input class="input" type="text" name="username"><br><br>
 
-	<label class="adjust" id="password">Password</label><br>
-	<input class="adjust" type="password" name="password" required><br><br>
+	<label id="password">Password</label><br>
+	<input class="input" type="password" name="password" required><br><br>
 
-	<input class="adjust" type="submit" name="submit" onclick="test(signup)"><br>
+	<input class="login" type="submit" name="submit" value="Login" onclick="test(signup)"><br>
 
 	<p align="center">Don't have an account, signup now? <a href="signup.php">Signup Here</a></p>
 
